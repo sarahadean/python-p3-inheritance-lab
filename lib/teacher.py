283c637faf1a536b1f019fa94f4ba45dedfe483a
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-26 18:17:21 -0500

    all tests passed

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,41 @@ from user import User
 
 import random
 
+# knowledge = [
+#     "str is a data type in Python",
+#     "programming is hard, but it's worth it",
+#     "JavaScript async web request",
+#     "Python function call definition",
+#     "object-oriented teacher instance",
+#     "programming computers hacking learning terminal",
+#     "pipenv install pipenv shell",
+#     "pytest -x flag to fail fast",
+# ]
+
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
     def teach(self):
-        pass
+    #    print(self.knowledge)
+       random_fact = random.randint(0, len(self.knowledge)-1)
+       return self.knowledge[random_fact]
+
+mrs_smith = Teacher("Mary", "Smith")
+# print(mrs_smith.knowledge)
+#successful
+
+# print(mrs_smith.teach())
+#successfully printed random thing from knowledge list
+
